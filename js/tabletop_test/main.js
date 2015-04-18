c0c98f2cda43f1f9c7009611feda0eecc78b48a7
crcommons <crcommons@gmail.com>
2015-04-18 10:03:30 -0400

    Additions to js
    makes the names from js guild appear in the dom

--- js/tabletop_test/main.js
+++ js/tabletop_test/main.js
@@ -13,17 +13,39 @@ window.onload = function() { init() };
   function showInfo(data, tabletop) {
     tabletopData = data;
       console.log(tabletopData);
+      selectNames();
   }
 
-  document.getElementById('myTestButton').onclick = function () {
+  // document.getElementById('myTestButton').onclick = function () {
 tabletopData = JSON.stringify(tabletopData);
 
 //select div by id
-  var selector = document.getElementById('insertData');
+//   var selector = document.getElementById('insertData');
 
-  //insert data into div
-selector.innerHTML = tabletopData;
-  }
+//   //insert data into div
+// selector.innerHTML = tabletopData;
+//   }
+
+  
+//varible to hold data
+function selectNames () {
+var attendanceNames = tabletopData.map(function (item) { 
+  return item["Name"]; 
+}).map(function (name) { 
+  return "<div>" + name + "</div>" 
+}).join('');
+document.getElementById('insert').innerHTML = attendanceNames;
+}
+//for loop through array
+
+  //select key name
+  //put key and value in variable
+  //wrap each value in a DIV tag
+  //add a class to each div
+
+
+
+//innerHTML the variable
 
   
 
